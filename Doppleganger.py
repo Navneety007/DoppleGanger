2cd16fbf06005749c4e88aab2a86079b4587f75d
Navneety007 <73738473+Navneety007@users.noreply.github.com>
2021-08-08 14:03:06 +0530

    Add files via upload

---
+++ Doppleganger.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import asyncpg
+
+class Doppleganger(commands.Bot):
+    def __init__(self,**options):
+        super().__init__(
+            command_prefix = self.determine_prefix, 
+            help_command=None, 
+            description="Bot with amazing Economy, Profile, Guild, welcome etc. commands",
+            case_insensitive = True,
+            intents = discord.Intents.all(),
+            owner_id = 779743087572025354,
+            allowed_mentions = discord.AllowedMentions(everyone = False, roles = False),
+             **options)
+
+        self.colors = [0x0dd2ff,0x03f5ff,0x2affa9,0x18e6ff,0x17ffc2,0x03f5ff,0x30e79d]
+    
+        self.loop.run_until_complete(self.create_db_pool())
+
+    async def determine_prefix(self, bot, message):
+        guild_id = message.guild.id
+        
+        prefix = await self.pg_con.fetchrow("SELECT prefix FROM guild WHERE guildid = $1",guild_id)
+        return [prefix[0]]
+
+    
+    async def create_db_pool(self):
+        #self.pg_con = await asyncpg.create_pool(database = "Code Stacks",user = "postgres",password = "12382692")
+        self.pg_con = await asyncpg.create_pool(os.environ['DATABASE_URL'],ssl = "require")
+        
---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from Doppleganger import Doppleganger
+import jishaku
+import cogs
+import decouple
+
+
+os.environ.setdefault("JISHAKU_HIDE", "1")
+os.environ.setdefault("JISHAKU_RETAIN", "1")
+os.environ.setdefault("JISHAKU_NO_UNDERSCORE", "1")
+
+
+if __name__ == "__main__":
+
+    bot = Doppleganger()
+
+    for name in os.listdir("./cogs"):   
+        if name.endswith(".py"):
+            bot.load_extension("cogs.{}".format(name[:-3]))
+
+    bot.load_extension('jishaku')
+    
+    bot.run(os.environ["TOKEN"])
+
+
